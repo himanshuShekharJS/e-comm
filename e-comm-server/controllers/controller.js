7898807f4776793afe060cd66607b1c242d7299a
Himanshu Shekhar <hsk.drm.2030@gmail.com>
2021-05-22 14:39:34 +0530

    implemented the initial logic of adding product to databse

--- e-comm-server/controllers/controller.js
+++ e-comm-server/controllers/controller.js
@@ -1,4 +1,27 @@
 //all the business logic here
-export const getHome = (req,res) =>{
-    res.status(200).send(`<h1>Hi Welcome to Home Page. Here We will have all our products Available!!</h1>`)
+//importing mongoDatabase here to create and add product
+import productDatabase from '../models/mongoDB.js'
+
+export const getHomePage = async(req,res) =>{
+   try {
+       const products =  await productDatabase.find();
+       console.log(products);
+    res.status(200).json(products)
+       
+   } catch (error) {
+       res.status(404).json({message: error.message})
+   }
+}
+
+export const addProduct = async(req,res) =>{
+    // const {title,price,ratings,category,selectedFiles} = req.body;
+    const product = req.body;
+
+    const newProduct = new productDatabase(product)
+try {
+    await newProduct.save();
+    res.status(201).json(newProduct)
+} catch (error) {
+    res.status(409).json({message:error.message})
+}
 }
