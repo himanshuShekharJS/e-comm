8dbd1fbd5081884ee1e622b3d4911e6f5fbe7a0b
Himanshu Shekhar <hsk.drm.2030@gmail.com>
2021-05-23 22:46:27 +0530

    Added Delete Functionality

--- e-comm-client/src/actions/productAction.js
+++ e-comm-client/src/actions/productAction.js
@@ -1,43 +1,146 @@
-import * as api from '../api/index.js'
-
+import * as api from "../api/index.js";
 
 //action creators
-import { ADD_NEW_PRODUCT, FETCH_ALL_PRODUCTS, UPDATE_THE_PRODUCT } from "./actionTypes"
+import {
+  ADD_NEW_PRODUCT,
+  DELETE_THE_PRODUCT,
+  FETCH_ALL_PRODUCTS,
+  FILTER_BY_MENS_WEAR,
+  FILTER_BY_WOMENS_WEAR,
+  UPDATE_THE_PRODUCT,
+  FILTER_BY_LAPTOPS,
+  FILTER_BY_MOBILES,
+  FILTER_BY_LOW_TO_HIGH,
+  FILTER_BY_HIGH_TO_LOW,
+  FILTER_BY_RANGE,
+  SEARCH
+} from "./actionTypes";
 
 //making use of redux-thunk
-export const getProducts = ()=> async(dispatch) => {
-    try {
-        const response = await api.fetchProducts();
-        console.log('Inside productAction.js File---->, response from the API after fetching all the product data',response);
-        const {data} = response;
-        dispatch({type:FETCH_ALL_PRODUCTS, payload:data})
-    } catch (error) {
-        console.log('Inside productAction.js File---->Error occured for getProducts() ',error.message);
-    }
-}
+export const getProducts = () => async (dispatch) => {
+  try {
+    const response = await api.fetchProducts();
+    console.log(
+      "Inside productAction.js File---->, response from the API after fetching all the product data",
+      response
+    );
+    const { data } = response;
+    dispatch({ type: FETCH_ALL_PRODUCTS, payload: data });
+  } catch (error) {
+    console.log(
+      "Inside productAction.js File---->Error occured for getProducts() ",
+      error.message
+    );
+  }
+};
 
-export const addNewProduct = (newProduct) => async(dispatch) => {
-try {
+export const addNewProduct = (newProduct) => async (dispatch) => {
+  try {
     const response = await api.createProduct(newProduct);
-    const {data} = response;
+    const { data } = response;
     dispatch({
-        type:ADD_NEW_PRODUCT, payload:data
-    })
-} catch (error) {
-    console.log('Inside productAction.js File---->Error occured for addNewProduct() ',error);
-}
-}
+      type: ADD_NEW_PRODUCT,
+      payload: data,
+    });
+  } catch (error) {
+    console.log(
+      "Inside productAction.js File---->Error occured for addNewProduct() ",
+      error
+    );
+  }
+};
 
-export const updateTheProduct = (id,prevProduct)=> async(dispatch) =>{
+export const updateTheProduct = (id, prevProduct) => async (dispatch) => {
+  try {
+    const updatetdProductinResponse = await api.updateProduct(id, prevProduct);
+    const { data } = updatetdProductinResponse;
+    dispatch({
+      type: UPDATE_THE_PRODUCT,
+      payload: data,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const deleteTheProduct = (id) => async (dispatch) => {
+  try {
+    await api.deleteProduct(id);
+    dispatch({
+      type: DELETE_THE_PRODUCT,
+      payload: id,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+export const filterBy = (category) => async (dispatch) => {
+  let actionTypes = "";
+  switch (category) {
+    case "mens_wear":
+      actionTypes = FILTER_BY_MENS_WEAR;
+      break;
+    case "womens_wear":
+      actionTypes = FILTER_BY_WOMENS_WEAR;
+      break;
+    case "laptops":
+      actionTypes = FILTER_BY_LAPTOPS;
+      break;
+    case "mobiles":
+      actionTypes = FILTER_BY_MOBILES;
+      break;
+    default:
+      return;
+  }
+  try {
+    const filteredProductInResponse = await api.filterProduct(category);
+    const { data } = filteredProductInResponse;
+    dispatch({
+      type: actionTypes,
+      payload: data,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+export const sortByPrice = (category) => async (dispatch) => {
+  let actionTypes = "";
+  if (category === "low_to_high") {
+    actionTypes = FILTER_BY_LOW_TO_HIGH;
+  } else actionTypes = FILTER_BY_HIGH_TO_LOW;
+
+  try {
+    const sortedResponse = await api.sortProduct(category);
+    const { data } = sortedResponse;
+    dispatch({
+      type: actionTypes,
+      payload: data,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const filterByRange = (minPrice,maxPrice) => async (dispatch) => {
+  console.log(minPrice, maxPrice);
+  try {
+      const inRangeResponse = await api.filterBetween(minPrice,maxPrice);
+      const {data} = inRangeResponse;
+    dispatch({
+      type: FILTER_BY_RANGE,
+      payload: data,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+export const searchAmong = (text) =>async(dispatch) =>{
     try {
-        console.log('Inside the updaetTheProduct');
-        console.log(id,prevProduct);
-        const updatetdProductinResponse = await api.updateProduct(id,prevProduct);
-        console.log('Inside the updateThe Product inside productAction.js---->',updatetdProductinResponse);
-        const {data} = updatetdProductinResponse;
-        console.log('Inside the updateThe Product inside productAction.js---->destructuring data---->',data);
+        const response = await api.searchAllProduct(text);
+        const{data} = response;
         dispatch({
-            type:UPDATE_THE_PRODUCT,payload:data
+            type:SEARCH,
+            payload:data
         })
     } catch (error) {
         console.log(error);
