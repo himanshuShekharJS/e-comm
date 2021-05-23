4f7d2f59f48d97d11747aa79793e57153d50eab3
Himanshu Shekhar <hsk.drm.2030@gmail.com>
2021-05-23 22:49:25 +0530

    Minor bug fixation

--- e-comm-client/src/api/index.js
+++ e-comm-client/src/api/index.js
@@ -4,6 +4,9 @@ const url = 'http://localhost:5000/';
 
 export const fetchProducts= ()=> axios.get(url);
 export const createProduct = (newProduct) => axios.post(url,newProduct)
-export const updateProduct = (id,updatedProduct) => {
-    console.log("Inside the updateProduct inAPI...in index.js--->API--->",id,updatedProduct);
-    return axios.patch(`${url}${id}`,updatedProduct)}
+export const updateProduct = (id,updatedProduct) => axios.patch(`${url}${id}`,updatedProduct)
+export const deleteProduct = (id) => axios.delete(`${url}${id}`);
+export const filterProduct = (category) => axios.get(`${url}${category}`);
+export const sortProduct = (category) => axios.get(`${url}sort/${category}`);
+export const filterBetween = (minPrice,maxPrice) => axios.get(`${url}range?min=${minPrice}&max=${maxPrice}`);
+export const searchAllProduct = (value) => axios.get(`${url}search/${value}`)
