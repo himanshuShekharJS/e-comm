3eeb6db51857b5d591d0deef24ca53dee30e7adc
Himanshu Shekhar <hsk.drm.2030@gmail.com>
2021-05-23 22:50:56 +0530

    Filter Functionality Added

--- e-comm-client/src/components/Product.js
+++ e-comm-client/src/components/Product.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import {
   EuiButtonIcon,
   EuiCard,
@@ -15,20 +15,12 @@ import {
   EuiDescriptionListTitle,
 } from "@elastic/eui";
 import "@elastic/eui/dist/eui_theme_light.css";
-
+import { deleteTheProduct } from "../actions/productAction.js";
 const Product = ({ setCurrentID }) => {
   const products = useSelector((state) => state.productReducer); //productReducer is the object storing the value being fetched from the backend and you can find it inside the rootReducer.js file where the state is being managed
   console.log("Inside Product .js---------->inside product()");
   console.log("Products fetched from the backend----->", products);
-  // const [editProductForm,setEditProductForm] = useState(false);
-
-  // const openCurrentProductDetails = (id) =>{
-  //   const selectedProduct = products.find((product)=>product._id===id);
-  //   console.log("InsideopenCurrentProductDetails() in Product.JS file--->On click edit",selectedProduct);
-    
-  // }
-
-
+  const dispatch = useDispatch();
 
   const onEditClickHandler = (id) => {
     console.log("YOu wanted to edit the Product??");
@@ -37,73 +29,82 @@ const Product = ({ setCurrentID }) => {
     // setEditProductForm(true);
     // openCurrentProductDetails(id);
   };
-  
+
   const onDeleteClickHandler = (id) => {
     console.log("YOu wanted to delete the Product??");
-    setCurrentID(id);
-
+    dispatch(deleteTheProduct(id));
   };
-  
 
-  const showProductDescription = (imageSrc,id,title,price) =>(
+  const showProductDescription = (imageSrc, id, title, price) => (
     <EuiFlexItem key={id}>
-          <EuiPanel>
-            <EuiFlexGrid columns={2}>
-              <EuiFlexItem grow={false}>
-                <span style={{ width: "12rem" }}>
-                  <EuiImage
-                    src={imageSrc}
-                    alt="Product Image"
-                    margin="s"
-                    size="fullWidth"
-                  />
-                </span>
-              </EuiFlexItem>
-              <EuiFlexItem grow={false}>
-              <EuiFlexItem grow={2}>
+      <EuiPanel>
+        <EuiFlexGrid columns={2}>
+          <EuiFlexItem grow={false}>
+            <span style={{ width: "12rem" }}>
+              <EuiImage
+                src={imageSrc}
+                alt="Product Image"
+                margin="s"
+                size="fullWidth"
+              />
+            </span>
+          </EuiFlexItem>
+          <EuiFlexItem grow={false}>
+            <EuiFlexItem grow={2}>
               <EuiDescriptionList>
-              <EuiDescriptionListTitle>
-                <EuiText>
-                  <h4>{title}</h4>
-                </EuiText>
-              </EuiDescriptionListTitle>
-              <EuiDescriptionListDescription>
-                <EuiText size='s'>₹{price}</EuiText>
-              </EuiDescriptionListDescription>
-            </EuiDescriptionList>
+                <EuiDescriptionListTitle>
+                  <EuiText>
+                    <h4>{title}</h4>
+                  </EuiText>
+                </EuiDescriptionListTitle>
+                <EuiDescriptionListDescription>
+                  <EuiText size="s">₹{price}</EuiText>
+                </EuiDescriptionListDescription>
+              </EuiDescriptionList>
+            </EuiFlexItem>
+            <EuiSpacer />
+            <EuiFlexGroup justifyContent="center">
+              <EuiFlexItem grow={false}>
+                <EuiButtonIcon
+                  color="primary"
+                  iconType="pencil"
+                  title="Edit"
+                  aria-label="Use aria labels when no actual label is in use"
+                  onClick={() => onEditClickHandler(id)}
+                />
               </EuiFlexItem>
-                <EuiSpacer />
-                <EuiFlexGroup justifyContent="center">
-                  <EuiFlexItem grow={false}>
-                    <EuiButtonIcon
-                      color="primary"
-                      iconType="pencil"
-                      title="Edit"
-                      aria-label="Use aria labels when no actual label is in use"
-                      onClick={()=>onEditClickHandler(id)}
-                    />
-                  </EuiFlexItem>
-                  <EuiFlexItem grow={false}>
-                    <EuiButtonIcon
-                      color="danger"
-                      iconType="trash"
-                      title="Delete"
-                      aria-label="Use aria labels when no actual label is in use"
-                      onClick={()=>onDeleteClickHandler(id)}
-                    />
-                  </EuiFlexItem>
-                </EuiFlexGroup>
+              <EuiFlexItem grow={false}>
+                <EuiButtonIcon
+                  color="danger"
+                  iconType="trash"
+                  title="Delete"
+                  aria-label="Use aria labels when no actual label is in use"
+                  onClick={() => onDeleteClickHandler(id)}
+                />
               </EuiFlexItem>
-            </EuiFlexGrid>
-          </EuiPanel>
-        </EuiFlexItem>
-  )
+            </EuiFlexGroup>
+          </EuiFlexItem>
+        </EuiFlexGrid>
+      </EuiPanel>
+    </EuiFlexItem>
+  );
   return (
     <>
       <EuiFlexGroup>
-        {products.map((product)=>(
-          showProductDescription(product.selectedFile,product._id,product.title,product.price)
-        ))}
+        {products?.length > 0 ? (
+          products.map((product) =>
+            showProductDescription(
+              product.selectedFile,
+              product._id,
+              product.title,
+              product.price
+            )
+          )
+        ) : (
+          <EuiText>
+            <h2>No Products Found</h2>
+            </EuiText>
+        )}
       </EuiFlexGroup>
     </>
   );
--- e-comm-client/src/reducers/productReducer.js
+++ e-comm-client/src/reducers/productReducer.js
@@ -1,20 +1,50 @@
-import { FETCH_ALL_PRODUCTS,ADD_NEW_PRODUCT, UPDATE_THE_PRODUCT } from "../actions/actionTypes";
+import {
+  FETCH_ALL_PRODUCTS,
+  ADD_NEW_PRODUCT,
+  UPDATE_THE_PRODUCT,
+  DELETE_THE_PRODUCT,
+  FILTER_BY_MENS_WEAR,
+  FILTER_BY_WOMENS_WEAR,
+  FILTER_BY_MOBILES,
+  FILTER_BY_LAPTOPS,
+  FILTER_BY_LOW_TO_HIGH,
+  FILTER_BY_HIGH_TO_LOW,
+  FILTER_BY_RANGE,
+  SEARCH,
+} from "../actions/actionTypes";
 export const initialState = [];
 
-
-
-const productReducer = (products=initialState, action) =>{
-switch (action.type) {
+const productReducer = (products = initialState, action) => {
+  switch (action.type) {
     case UPDATE_THE_PRODUCT:
-        console.log("Inside the productReducer---After clciking the dit button -->Dispatched the UPDATE THE PRODUCT",products);
-        return products.map((p)=> p._id===action.payload._id ? action.payload : p)
+      return products.map((p) =>
+        p._id === action.payload._id ? action.payload : p
+      );
     case FETCH_ALL_PRODUCTS:
-        return action.payload;
+      return action.payload;
     case ADD_NEW_PRODUCT:
-        return [...products,action.payload];
+      return [...products, action.payload];
+    case DELETE_THE_PRODUCT:
+      return products.filter((p) => p._id !== action.payload);
+    case FILTER_BY_MENS_WEAR:
+      return action.payload;
+    case FILTER_BY_WOMENS_WEAR:
+      return action.payload;
+    case FILTER_BY_MOBILES:
+      return action.payload;
+    case FILTER_BY_LAPTOPS:
+      return action.payload;
+    case FILTER_BY_LOW_TO_HIGH:
+      return action.payload;
+    case FILTER_BY_HIGH_TO_LOW:
+      return action.payload;
+    case FILTER_BY_RANGE:
+      return action.payload;
+    case SEARCH:
+      return action.payload;
     default:
-        return products;
-}
-}
+      return products;
+  }
+};
 
 export default productReducer;
